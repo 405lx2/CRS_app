f8a2b3493f27e97f022973b045cb872ded7e38d8
405lx2 <405lx2@163.com>
2016-04-03 20:41:07 +0800

    Test Git

---
+++ JudgeLoginInfo.java
@@ -0,0 +1,29 @@
+package com.example.lx.crs_app;
+
+/**
+ * Created by 405lx on 2016/4/3.
+ */
+public class JudgeLoginInfo {
+    private String Password;
+    private String UserName;
+
+    public String getUserName() {
+        return UserName;
+    }
+
+    public String getPassword() {
+        return Password;
+    }
+
+    public void setUserName(String userName) {
+        UserName = userName;
+    }
+
+    public void setPassword(String password) {
+        Password = password;
+    }
+
+
+
+
+}
